a06700d5fd2d235fd6812f9349be8569e911551f
Nikolay Larin <nikolay.larin.itmail@gmail.com>
2019-03-19 18:46:44 +0200

    Replaced tabs with spaces and fixed mentors remarks

--- Lesson_01/Variable.java
+++ Lesson_01/Variable.java
@@ -1,20 +1,21 @@
 public class Variable {
-	public static void main (String[] args) {
-		char windowsEdition = '7';
-		byte ram = 3;
-		short systemType = 64;
-		float cpuFrq = 2.2f;
-		long coreNumb = 2;
-		int servPack = 1;
-		double hdCap = 500;
-		boolean touchpad = false;
-		System.out.println("Short description of my computer:");
-		System.out.println("- OS: Windows "+ windowsEdition + ", Service Pack " + servPack);
-		System.out.println("- OS type: " + systemType + "-bit");
-		System.out.println("- RAM capacity: " + ram + " Gb");
-		System.out.println("- CPU frequency: " + cpuFrq + " GHz");
-		System.out.println("- CPU cores: " + coreNumb);
-		System.out.println("- HDD capacity: " + hdCap + " Gb");
-		System.out.println("- Touchpad availability: " + touchpad);
-		}
-}	
+    public static void main(String[] args) {
+        char windowsEdition = '7';
+        byte ram = 3;
+        short systemType = 64;
+        float cpuFrq = 2.2f;
+        long coreNumb = 2;
+        int servPack = 1;
+        double hdCap = 500;
+        boolean touchpad = false;
+
+        System.out.println("Short description of my computer:");
+        System.out.println("- OS: Windows "+ windowsEdition + ", Service Pack " + servPack);
+        System.out.println("- OS type: " + systemType + "-bit");
+        System.out.println("- RAM capacity: " + ram + " Gb");
+        System.out.println("- CPU frequency: " + cpuFrq + " GHz");
+        System.out.println("- CPU cores: " + coreNumb);
+        System.out.println("- HDD capacity: " + hdCap + " Gb");
+        System.out.println("- Touchpad availability: " + touchpad);
+    }
+}   
