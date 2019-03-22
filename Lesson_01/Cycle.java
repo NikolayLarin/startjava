73caa382eb5064b1e67e2c23ed217da24bb0575f
Nikolay Larin <nikolay.larin.itmail@gmail.com>
2019-03-22 11:57:41 +0200

    Fixed formatting and improved code

--- Lesson_01/Cycle.java
+++ Lesson_01/Cycle.java
@@ -1,36 +1,32 @@
 public class Cycle {
 
-    public static void main(String[] args) {
-        System.out.println ();        
-        for (short i = 0; i <= 20; i++) {
-            System.out.print (i +"; ");
+    public static void main(String[] args) {      
+        for (int i = 0; i <= 20; i++) {
+            System.out.print(i + "; ");
             if (i == 12){
                 System.out.println();
             }
         }
-        System.out.println();
-        System.out.println();
+        System.out.println("\n");
 
         int i = -6;
-        while (i <= 6){
+        while (i <= 6) {
             System.out.print(i + "; ");
-            i = i + 2;
+            i += 2;
         }
-        System.out.println();
-        System.out.println();
+        System.out.println("\n");
 
-        int j = 10;
-        int a = 0;
-        System.out.print("The sum of such odd numbers ");
+        int rangeStart = 10;
+        int sumOddNumbers = 0;
+        System.out.print("The sum of such odd numbers: ");
         do {                    
-            if (j % 2 !=0) {
-                a += j;                
-                System.out.print(j + " ");
+            if (rangeStart % 2 !=0) {
+                sumOddNumbers += rangeStart;                
+                System.out.print(rangeStart + "; ");
             }
-            j++;
-        } while (j <= 20);
-        System.out.println ();
-        System.out.println("is " + a);
-        System.out.println ();
+            rangeStart++;
+        } while (rangeStart <= 20);
+        System.out.println();
+        System.out.println("is " + sumOddNumbers + "\n");
     }
 }
