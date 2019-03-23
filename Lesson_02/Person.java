a92de0436bd0c03901d17d168a3a0ee80128c798
NikolayLarin <nikolay.larin.itmail@gmail.com>
2019-03-23 17:29:05 +0200

    Recoded Person methods.

--- Lesson_02/Person.java
+++ Lesson_02/Person.java
@@ -1,28 +1,29 @@
 public class Person {
 
     String sex = "male";
-    String name = "Victor"
+    String name = "Victor";
     float height = 1.78f;
     float weight = 88.6f;
-    int ege = 39;
+    int age = 39;
 
     void go() {
-
+        System.out.println("I go.");
     }
 
     void sit() {
-
+        System.out.println("I'm sitting.");
     }
 
     void run() {
-
+        System.out.println("I'm runing.");
     }
 
     String speak() {
-
+        return "I'm talking now.";
     }
 
     boolean learnJava() {
-
+        System.out.println("Yes, I'm learning Java now.");
+        return true;
     }
 }
