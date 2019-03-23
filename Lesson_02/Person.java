b3804e633f046afdbafbf695a6cd67943f23a54b
NikolayLarin <nikolay.larin.itmail@gmail.com>
2019-03-23 16:26:48 +0200

    Implement class Person in HW-2

---
+++ Lesson_02/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+
+    String sex = "male";
+    String name = "Victor"
+    float height = 1.78f;
+    float weight = 88.6f;
+    int ege = 39;
+
+    void go() {
+
+    }
+
+    void sit() {
+
+    }
+
+    void run() {
+
+    }
+
+    String speak() {
+
+    }
+
+    boolean learnJava() {
+
+    }
+}
