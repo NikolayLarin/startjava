c2892793b8c20a265e6a62a147b70fbf31825e9e
NikolayLarin <nikolay.larin.itmail@gmail.com>
2019-03-24 13:30:28 +0200

    Added getters and setters

--- Lesson_02/Wolf.java
+++ Lesson_02/Wolf.java
@@ -1,28 +1,70 @@
 public class Wolf {
 
-    String sex;
-    String name;
-    int age;
-    String coloring;
+    private String sex;
+    private String name;
+    private int age;
+    private String coloring;
 
-    void go() {
+    public String getSex() {
+        return sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getColoring() {
+        return coloring;
+    }
+
+    void setSex(String sex) {
+        if (sex == "male" || sex == "shemale") {
+            this.sex = sex;
+        } else {
+            System.out.println("Incorrect sex, try again.");
+            System.exit(0);         
+        }
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+    
+    void setAge(int age) {
+        if (age > 8 || age <= 0) {
+            System.out.println("Incorrect age, try again.");
+            System.exit(0);
+        } else {
+            this.age = age;
+        }
+    }
+
+    public void setColoring(String coloring) {
+        this.coloring = coloring;
+    }
+
+    public void go() {
         System.out.println("I go now.");
     }
 
-    void sit() {
+    public void sit() {
         System.out.println("I'm sitting now.");
     }
 
-    void run() {
+    public void run() {
         System.out.println("I'm runing now.");
     }
 
-    String howl() {
+    public String howl() {
         System.out.println("Wooooo!");
         return "Woo-Woooo-ooo!";
     }
 
-    void hunt() {
+    public void hunt() {
         System.out.println("I'm hunting now.");
     }
 }
