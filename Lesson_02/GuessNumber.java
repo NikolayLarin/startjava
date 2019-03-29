b0c3610b9690267dd1c62e77bbc587b6a328d4bb
Nikolay Larin <nikolay.larin.itmail@gmail.com>
2019-03-29 20:10:26 +0200

    Implement ==Guess number== game fo two

--- Lesson_02/GuessNumber.java
+++ Lesson_02/GuessNumber.java
@@ -1,18 +1,48 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class GuessNumber {
 
     private Random random0_100 = new Random();
     private int hiddenNumber = random0_100.nextInt(101);
+    private Scanner scanInput = new Scanner(System.in);
+    private int playerOneNumber;
+    private int playerTwoNumber;
 
-    public void guessNumber(String playerName, int inputNumber) {
-        if (inputNumber > hiddenNumber) {
+    public GuessNumber(String playerOneName, String playerTwoName) {
+        char userChoice;
+        do {
+            do {
+                playerOneNumber = setPlayerNumber(playerOneName);
+                numberVersusHidden(playerOneName, playerOneNumber);
+                playerTwoNumber = setPlayerNumber(playerTwoName);
+                numberVersusHidden(playerTwoName, playerTwoNumber);
+                System.out.print("\nDo you want to continue? (Y/N): ");
+                userChoice = scanInput.next().charAt(0);
+            } while (userChoice != 'Y' && userChoice != 'N');
+        } while (userChoice == 'Y');
+    }
+
+    private int setPlayerNumber(String playerName) {
+        int playerNumber;
+        do {
+            System.out.print("\n" + playerName +", enter your number: ");
+            playerNumber = scanInput.nextInt();
+                if (playerNumber < 0 | playerNumber > 100) {
+                    System.out.println("You entered incorrect number. \nIt must be from 0 to 100. \nTry again!");
+                }
+        } while (playerNumber < 0 | playerNumber > 100);
+        return playerNumber;
+    }
+
+    private void numberVersusHidden(String playerName, int playerNumber) {
+        if (playerNumber > hiddenNumber) {
             System.out.println("Entered number is more than hidden.");
-        } else if (inputNumber < hiddenNumber) {
+        } else if (playerNumber < hiddenNumber) {
             System.out.println("Entered number is less than hidden.");
         } else {            
             System.out.println("\n!!!!!!!!!!!!!!!!\nCongratulations, " + playerName + "! You are a winner!");
-            System.out.println("The hidden number is " + inputNumber + ".\n!!!!!!!!!!!!!!!!");
+            System.out.println("The hidden number is " + playerNumber + ".\n!!!!!!!!!!!!!!!!");
         }
     }
 }
--- Lesson_02/Player.java
+++ Lesson_02/Player.java
@@ -5,29 +5,9 @@ public class Player {
     private String playerName;
     private int playerNumber;
     private Scanner scanInput = new Scanner(System.in);
-
-    public void player() {
-        playerName = scanInput.next();
-        System.out.println("Hallo, " + playerName + "!");
-    }
-
-    public void setPlayerName(String playerName) {
+    
+    public Player(String playerName) {
         this.playerName = playerName;
-    }
-
-    public String getPlayerName() {
-        return playerName;
-    }
-
-    public int setPlayerNumber() {
-        do {
-            System.out.print("\n" + playerName +", enter your number: ");
-            playerNumber = scanInput.nextInt();
-                if (playerNumber < 0 | playerNumber > 100) {
-                    System.out.println("You entered incorrect number. \nIt must be from 0 to 100. \nTry again!");
-                }
-        } while (playerNumber < 0 | playerNumber > 100);
-        this.playerNumber = playerNumber;
-        return playerNumber;
+        System.out.println("Hallo, " + playerName + "!");
     }
 }
