9587072a821afe4a87516a7a78db5226805585c6
Nikolay Larin <nikolay.larin.itmail@gmail.com>
2019-04-01 13:43:16 +0300

    Reimplement Guess Number game

--- Lesson_02/Player.java
+++ Lesson_02/Player.java
@@ -5,13 +5,9 @@ public class Player {
     private String playerName;
     private int playerNumber;
     private Scanner scanInput = new Scanner(System.in);
-    
+
     public Player(String playerName) {
         this.playerName = playerName;
-    }
-
-    public String getPlayerName() {
-        return playerName;
     }
 
     public void setPlayerNumber(String playerName) {
