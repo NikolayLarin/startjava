2751159f3a8b91d7e6726c89ec2deb41acece194
NikolayLarin <nikolay.larin.itmail@gmail.com>
2019-04-14 20:13:53 +0300

    Change sql files in lesson 5

--- src/com/startjava/Lesson_05/sql_intro/_clear_table_db.sql
+++ src/com/startjava/Lesson_05/sql_intro/_clear_table_db.sql
@@ -2,6 +2,7 @@
 -- \i _clear_table_db.sql
 
 \c jaegers;
+
 DELETE FROM jaegers;                             -- очистить всю таблицу
-ALTER SEQUENCE jaegers_id_seq RESTART WITH 1;    -- назначить автоинеркменту единицу
+ALTER SEQUENCE jaegers_id_seq RESTART WITH 1;    -- назначить автоинкременту единицу
 SELECT * FROM jaegers;                           -- вывести все строки таблицы
